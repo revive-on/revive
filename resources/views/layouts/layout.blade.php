cafb8139591d39a1824d93528c032f040fde8265
Velmont <dragond7@naver.com>
2018-04-02 22:57:37 +0900

    템플릿 추가

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>WebGame - @yield('title')</title>
+</head>
+<body>
+@section('sidebar')
+
+@endsection
+
+<div class="container">
+    @yield('content')
+</div>
+
+</body>
+</html>
